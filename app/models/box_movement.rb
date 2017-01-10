b19d9ff54f1ac3463545f285bdd4c80b54f1c428
Daniel Fernández <dfernandez@codigodelsur.com>
2017-01-10 17:38:47 -0300

    change box movement query

--- app/models/box_movement.rb
+++ app/models/box_movement.rb
@@ -13,7 +13,8 @@ class BoxMovement < ActiveRecord::Base
   def self.by_month_year(days,months)
     days = days.blank? ? DAYS : days.split(",")
     months = months.blank? ? MONTH : months.split(",")
-    BoxMovement.where("cast(strftime('%m', date) as int) IN (?) AND cast(strftime('%d', date) as int) IN (?) ", months , days).order(:date)
+    BoxMovement.where("Extract(month from date) IN (?) AND Extract(day from date) IN (?) ", months , days).order(:date)
+
   end
 
 
